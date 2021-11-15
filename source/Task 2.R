5e5c79204c921fbc063cdc81af60b19a1d94d873
Hugo-Andres-Sabogal-Perez <hugosabo2001@hotmail.com>
2021-11-15 14:52:23 -0500

    Avances punto 1

--- source/Task 2.R
+++ source/Task 2.R
@@ -2,3 +2,26 @@
 # Fecha de elaboracion: 1/10/2021
 # Ultima modificacion: 3/10/2021
 # Version de R: 4.1.1
+
+
+
+rm(list = ls())
+if(!require(pacman)) install.packages(pacman) 
+if(!require(pacman)) install.packages(pacman)
+require(pacman)
+p_load(tidyverse,rio,skimr,readxl,haven,WriteXLS)
+Sys.setlocale("LC_CTYPE", "en_US.UTF-8")
+
+#=========#
+# Punto 1 #
+#=========#
+
+
+
+chip=list()
+archivos=0
+ruta_excel= lapply(2017:2020, function(ano) list.files(poste0("data/imput/",ano), full.names=TRUE) %>% unlist()
+                   while (archivos<=80) {
+                     read_excel(ruta_excel)}
+                   
+                   
