e79ba7efc9ce006965964e50b38ea29684abc7e5
Hugo-Andres-Sabogal-Perez <hugosabo2001@hotmail.com>
2021-11-15 22:40:44 -0500

    avance punto 1 y 2

--- source/Task 2.R
+++ source/Task 2.R
@@ -16,12 +16,35 @@ Sys.setlocale("LC_CTYPE", "en_US.UTF-8")
 # Punto 1 #
 #=========#
 
+ruta = lapply(2017:2020 , function(año) list.files(paste0("data/imput/", año),full.names=T)) %>% unlist()
+                
+chip = list()
+for (i in 1:length(ruta)){
+  chip[[i]] = import(file = ruta[i])
+}
+                   
+View(chip)
+
+#=========#
+# Punto 2 #
+#=========#
 
 
-chip=list()
-archivos=0
-ruta_excel= lapply(2017:2020, function(ano) list.files(poste0("data/imput/",ano), full.names=TRUE) %>% unlist()
-                   while (archivos<=80) {
-                     read_excel(ruta_excel)}
-                   
-                   
+extraer = function(n,lista,tipo_rubro){
+  df = data.frame(valor=NA,cod_dane=NA,periodo=NA)
+  
+  lista_n = lista[[n]]
+  colnames(lista_n) = lista_n[7,]
+  df$valor= lista_n %>% subset(NOMBRE==tipo_rubro) %>% select(`PAGOS(Pesos)`)
+  
+  df$cod_dane= lista_n
+  
+  return(df)
+  
+  
+}
+
+extraer(n=10 , lista = chip , tipo_rubro = "SALUD" )
+
+
+
